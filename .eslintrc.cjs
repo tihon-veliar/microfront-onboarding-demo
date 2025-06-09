ce3ed666244c8d076ac4b7f4c76a0f3d9141de51
Tykhon Savchuk <tykhon.savchuk@valtech.com>
2025-06-09 13:46:47 +0200

    initial setup with Vite + React + TS + Chakra UI + Router + ESLint

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'prettier'
+  ],
+  plugins: ['react', 'react-hooks', 'jsx-a11y', 'import'],
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+  },
+};
